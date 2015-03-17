1c6b8ee53eb3230a4865823663dec6c81914b21f
Robert Holak <rholak@norcalmutual.com>
2015-03-17 11:46:28 -0500

    change font

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ main.on('click', 'select', function(e) {
   var textfield = new UI.Text({
     position: new Vector2(0, 50),
     size: new Vector2(144, 90),
-    font: 'gothic-12-bold',
+    font: 'gothic-14',
     text: 'Text Anywhere! Or Everywhere! Holy Crap!',
     textAlign: 'center'
   });
